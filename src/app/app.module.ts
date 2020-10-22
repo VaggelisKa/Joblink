0338486ffef1134537f276f8945db9ac0573b81a
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-22 13:46:18 +0300

    Adding home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-   declarations: [
+   declarations: [	
       AppComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
