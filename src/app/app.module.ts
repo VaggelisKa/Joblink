914285a7dcaf59b209ea58fef8e98ca9246f4536
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-23 15:06:38 +0300

    Adding shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './auth/components/nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
    declarations: [
@@ -26,10 +26,7 @@ import { ToastrModule } from 'ngx-toastr';
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot(),
-      ToastrModule.forRoot({
-         positionClass: 'toast-bottom-right'
-      })
+      SharedModule
    ],
    providers: [],
    bootstrap: [
