9af7612dac63359c13a87b502d390e2057af3f94
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-23 13:05:11 +0300

    Adding auth guard to all neccessary routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,16 @@ import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    {path: 'members/:id', component: MemberDetailComponent},
-    {path: 'lists', component: ListsComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'lists', component: ListsComponent},
+        ]
+    },
     {path: 'messages', component: MessagesComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
