2f79db6bbfb39c11078b874d8d852b3b670f4d2e
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-28 11:59:16 +0200

    Creating MemberService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
+import { Observable, ReplaySubject, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../models/user';
+
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly baseUrl = 'https://localhost:5001/api/account/';
+  private readonly baseUrl = environment.apiUrl;
 
   private _userSource = new ReplaySubject<User>(1);
   currentUser$ = this._userSource.asObservable();
@@ -17,7 +19,7 @@ export class AuthService {
 
   login(model: any): Observable<any> {
     return this._http
-      .post<{token: string}>(this.baseUrl + 'login', model)
+      .post<{token: string}>(this.baseUrl + 'account/login', model)
       .pipe(
         map((response: User) => {
           const user = response;
@@ -30,7 +32,7 @@ export class AuthService {
 
   register(model: any): Observable<any> {
     return this._http
-      .post(this.baseUrl + 'register', model)
+      .post(this.baseUrl + 'account/register', model)
       .pipe(
         map((user: User) => {
           if (user) {
---
+++ src/app/members/members.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Member } from '../models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer' + JSON.parse(localStorage.getItem('CurrentUser')).token
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  private readonly baseUrl = environment.apiUrl;
+
+  constructor(private _router: Router,
+              private _http: HttpClient) { }
+
+  getMembers(): Observable<Member[]> {
+    return this._http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMemberByUsername(username: string): Observable<Member> {
+    return this._http.get<Member>(this.baseUrl + `users/${username}`, httpOptions);
+  }
+}
