0734a9a1e97e5efdba7c4d143975f465fdafcafc
VaggelisKa <e.karavasileiadis@gmail.com>
2020-11-07 20:10:14 +0200

    Displaying main image thummbnail in the navbar

--- src/app/interceptors/loading.interceptor.ts
+++ src/app/interceptors/loading.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BusyService } from '../shared/services/busy.service';
-import { delay, finalize } from 'rxjs/operators';
+import { finalize } from 'rxjs/operators';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -17,7 +17,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this._busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => {
         this._busyService.idle();
       })
